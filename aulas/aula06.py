97dbd73c9fbd7612b19a1ae03c3cfc5de2913668
Bruno Eduardo de Carvalho Lucena <57199262+xBerunoh@users.noreply.github.com>
2025-03-30 13:33:42 -0400

    Aula 06 - tipos primitivos

---
+++ aulas/aula06.py
@@ -0,0 +1,14 @@
+# TIPOS PRIMITIVOS
+# int , float , bool , str
+'''
+int {7,-4,0,9875}
+float {4.5 , 0.076 , -15.223 , 7.0 }
+bool {true , false}
+str {'Olá , '7.5' , ''}
+
+'''
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+
+s = n1+n2
+print('A soma de {} com {} é {}'.format(n1,n2,s))
