b0e6930c0d680ee037dd774f434d229862ba361b
Bruno Eduardo de Carvalho Lucena <brunoeduardo92093676@gmail.com>
2025-04-09 16:44:21 -0400

    Algumas funções a mais da função is ...

--- desafios/desafio004.py
+++ desafios/desafio004.py
@@ -1,9 +1,12 @@
 # FAÇA UM PROGRAMA QUE LEIA ALGO PELO TECLADO E MOSTRE NA TELA O SEU TIPO PRIMITIVO E TODAS AS INFORMAÇÕES POSSIVELS
 n = (input("Digite algo: "))
 
-print('valor {} é um número inteiro ?',n.isnumeric())
+print('valor {} é um número ?',n.isnumeric())
 print('valor {} possui letras alfabeticas ?',n.isalpha())
+print('valor {} é alpha númerico?',n.isalnum())
 print('valor {} compostas de letras minusculas ?',n.islower())
 print('valor {} compostas de letras maiusculas ?',n.isupper())
 print('valor {} todas os caracteres da string são decimais ?',n.isdecimal())
+print('valor {} só tem espaços?',n.isspace())
+
 
