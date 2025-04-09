02e2ffaf8926a52915d0ce163bb74fbf6feeefdb
Bruno Eduardo de Carvalho Lucena <brunoeduardo92093676@gmail.com>
2025-04-09 17:15:15 -0400

    Exemplos dos operadores aritmeticos !

--- aulas/aula07.py
+++ aulas/aula07.py
@@ -1,8 +1,11 @@
 #Operação Aritmeticas
 '''
-+ - soma
-- - subtração
-* - multiplicação
-/ - divisão
-% - resto da divisão
+OPERADORES ARITMETICOS
++ -> soma Ex.: 5+2 == 7
+- -> subtração Ex.: 5-2 == 3
+* -> multiplicação Ex.: 5*2 == 10
+/ -> divisão Ex.: 5/2 == 2.5
+% -> resto da divisão Ex.: 5%2 == 1
+// -> Número inteiro da divisão Ex.: 5 // 2 == 2
+** -> Potencia Ex.: 5**2 == 25
 '''
